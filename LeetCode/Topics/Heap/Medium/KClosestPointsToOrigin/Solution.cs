a0c3e843f29eebe779d4dc09d5d218fe1a02f426
Andrei Kovtun <andreykpq@gmail.com>
2024-08-18 16:30:05 +0200

    initial commit

---
+++ LeetCode/Topics/Heap/Medium/KClosestPointsToOrigin/Solution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Topics.Heap.Medium.KClosestPointsToOrigin;
+
+public class Solution
+{
+    public int[][] KClosest(int[][] points, int k)
+    {
+        var kClosestPoints = new int[k][];
+        var pointsOrderedByDistance = new SortedDictionary<int, List<int[]>>();
+        
+        foreach (var point in points)
+        {
+            var distance = point[0] * point[0] + point[1] * point[1];
+
+            if (pointsOrderedByDistance.ContainsKey(distance))
+            {
+                pointsOrderedByDistance[distance].Add(point);
+            }
+            else
+            {
+                var points_ = new List<int[]>();
+                points_.Add(point);
+                pointsOrderedByDistance.Add(distance, points_);
+            }
+        }
+
+        var count = 0;
+        foreach (var distanceDotsPair in pointsOrderedByDistance)
+        {
+            for (int i = count; i < k && i < count + distanceDotsPair.Value.Count; i++)
+                kClosestPoints[i] = distanceDotsPair.Value[i - count];
+            
+            count += distanceDotsPair.Value.Count;
+            
+            if(count >= k) break;
+        }
+
+        return kClosestPoints;
+    }
+}
---
+++ LeetCode/Topics/Heap/Medium/KClosestPointsToOrigin/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Topics.Heap.Medium.KClosestPointsToOrigin;
+
+public class Tests
+{
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]{ (int[][])[[3,3],[5,-1],[-2,4]], 2, (int[][])[[3,3],[-2,4]]}
+        };
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void CorrectResult(int[][] points, int k, int[][] expectedResult)
+    {
+        var solution = new Solution();
+        var actualResult = solution.KClosest(points, k);
+        
+        Assert.True(
+            !expectedResult.Except(actualResult).Any()
+            && expectedResult.Length == actualResult.Length);
+    }
+}

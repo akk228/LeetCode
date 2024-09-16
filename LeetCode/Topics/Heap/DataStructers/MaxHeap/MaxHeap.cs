fcef2f22f473ee8ee42b83e8f3cb33e41e059860
Andrei Kovtun <andreykpq@gmail.com>
2024-09-16 23:06:07 +0200

    Adds MaxHeap and test ensuring correctness of building heap

---
+++ LeetCode/Topics/Heap/DataStructers/MaxHeap/MaxHeap.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Topics.Heap.DataStructers.MaxHeap;
+
+public class MaxHeap
+{
+    private readonly List<int> _heap;
+
+    public MaxHeap()
+    {
+        _heap = new List<int>();
+    }
+
+    public MaxHeap(List<int> nums)
+    {
+        _heap = new List<int>();
+        foreach (var num in nums)
+        {
+            _heap.Add(num);
+            HeapUp();
+        }
+    }
+
+    public List<int> GetHeap => _heap.ToList();
+    
+    private void HeapUp()
+    {
+        var index = _heap.Count - 1;
+        while (GetParent(index) >= 0 && _heap[index] > _heap[GetParent(index)])
+        {
+            var parentIndex = GetParent(index);
+            if (_heap[parentIndex] < _heap[index])
+            {
+                (_heap[parentIndex], _heap[index]) = (_heap[index], _heap[parentIndex]);
+                index = parentIndex;
+            }
+        }
+    }
+
+    public static int GetLeftChild(int parentIndex)
+    {
+        return 2*parentIndex + 1;
+    }
+    
+    public static int GetRigthChild(int parentIndex)
+    {
+        return 2*(parentIndex + 1);
+    }
+
+    public static int GetParent(int childIndex)
+    {
+        return (childIndex - 1) / 2;
+    }
+}
---
+++ LeetCode/Topics/Heap/DataStructers/MaxHeap/Tests/WhenBuildingHeap.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Topics.Heap.DataStructers.MaxHeap.Tests;
+
+public class WhenBuildingHeap
+{
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>()
+        {
+            new []{ new []{1, 2, 3, 4}},
+            new[]{ new [] {8,3,3,9,5}}
+        };
+    
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void ShouldBuildCorrectHeap(int[] arr)
+    {
+        var heap = new MaxHeap(arr.ToList());
+        
+        CheckHeapIsCorrect(0, heap.GetHeap);
+    }
+
+    private void CheckHeapIsCorrect(int node, List<int> heap)
+    {
+        if (node >= heap.Count)
+        {
+            Assert.True(true);
+            return;
+        }
+        int leftChild = MaxHeap.GetLeftChild(node);
+        int rightChild = MaxHeap.GetRigthChild(node);
+        
+        if((leftChild < heap.Count && heap[node] < heap[leftChild])
+           || ( rightChild < heap.Count && heap[node] < heap[rightChild]))
+            Assert.True(false);
+        
+        CheckHeapIsCorrect(MaxHeap.GetLeftChild(node), heap);
+        CheckHeapIsCorrect(MaxHeap.GetRigthChild(node), heap);
+    }
+}

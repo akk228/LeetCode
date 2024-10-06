099f5d1c16d284d3b352eca7e915f815b4276cf2
Andrei Kovtun <andreykpq@gmail.com>
2024-10-06 10:14:59 +0200

    Improves memory for word Search

--- LeetCode/Topics/Graphs/Medium/WordSearch/Solution.cs
+++ LeetCode/Topics/Graphs/Medium/WordSearch/Solution.cs
@@ -5,25 +5,19 @@
 /// </summary>
 /// <remarks>
 ///     Beats 46.33% in runtime
-///     Beats 48.32% in memory
+///     Beats 91.51% in memory
 /// </remarks>
 public class Solution
 {
     private readonly int[][] _shifts = [[0, 1], [0,-1], [1, 0], [-1, 0]];
-
+    private readonly char Null = '\0';
+    
     public bool Exist(char[][] board, string word) {
-        var visitedCells = new bool[board.Length][];
-        
-        for (var row = 0; row < board.Length; row++)
-        {
-            visitedCells[row] = new bool[board[0].Length];
-        }
-        
         for(var row = 0; row < board.Length; row++){
             for(var col = 0; col < board[0].Length; col++)
             {
                 if (board[row][col] != word[0]) continue;
-                if(Traverse(board, row, col, word, 0, visitedCells)){
+                if(Traverse(board, row, col, word, 0)){
                     return true;
                 }
             }    
@@ -32,7 +26,7 @@ public class Solution
         return false;
     }
 
-    private bool Traverse(char[][] board, int x, int y, string word, int currentPosition, bool[][] visitedCells){
+    private bool Traverse(char[][] board, int x, int y, string word, int currentPosition){
         if(currentPosition == word.Length - 1 && word[currentPosition] == board[x][y]){
             return true;
         }
@@ -41,7 +35,9 @@ public class Solution
             return false;
         }
 
-        visitedCells[x][y] = true;
+        var currentChar = board[x][y];
+        
+        board[x][y] = Null; // use unicode null to backtrack visited cells
         currentPosition++;
 
         var result = false;
@@ -50,13 +46,13 @@ public class Solution
             var x_ = x + shift[0];
             var y_ = y + shift[1];
 
-            if(x_ >= 0 && x_ < board.Length && y_ >=0 && y_ < board[0].Length && !visitedCells[x_][y_])
+            if(x_ >= 0 && x_ < board.Length && y_ >=0 && y_ < board[0].Length && board[x_][y_] != Null)
             {
-                result |= Traverse(board, x_, y_, word, currentPosition, visitedCells);
+                result |= Traverse(board, x_, y_, word, currentPosition);
             }
         }
         
-        visitedCells[x][y] = false;
+        board[x][y] = currentChar;
 
         return result;
     }

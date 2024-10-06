ee56fba312ea577de588992b1846fa934703e9f0
Andrei Kovtun <andreykpq@gmail.com>
2024-10-06 10:09:15 +0200

    WordSearch problem

---
+++ LeetCode/Topics/Graphs/Medium/WordSearch/Solution.cs
@@ -0,0 +1,63 @@
+﻿namespace LeetCode.Topics.Graphs.Medium.WordSearch;
+
+/// <summary>
+/// 79. Word Search
+/// </summary>
+/// <remarks>
+///     Beats 46.33% in runtime
+///     Beats 48.32% in memory
+/// </remarks>
+public class Solution
+{
+    private readonly int[][] _shifts = [[0, 1], [0,-1], [1, 0], [-1, 0]];
+
+    public bool Exist(char[][] board, string word) {
+        var visitedCells = new bool[board.Length][];
+        
+        for (var row = 0; row < board.Length; row++)
+        {
+            visitedCells[row] = new bool[board[0].Length];
+        }
+        
+        for(var row = 0; row < board.Length; row++){
+            for(var col = 0; col < board[0].Length; col++)
+            {
+                if (board[row][col] != word[0]) continue;
+                if(Traverse(board, row, col, word, 0, visitedCells)){
+                    return true;
+                }
+            }    
+        }
+
+        return false;
+    }
+
+    private bool Traverse(char[][] board, int x, int y, string word, int currentPosition, bool[][] visitedCells){
+        if(currentPosition == word.Length - 1 && word[currentPosition] == board[x][y]){
+            return true;
+        }
+
+        if(word[currentPosition] != board[x][y]){
+            return false;
+        }
+
+        visitedCells[x][y] = true;
+        currentPosition++;
+
+        var result = false;
+
+        foreach(var shift in _shifts){
+            var x_ = x + shift[0];
+            var y_ = y + shift[1];
+
+            if(x_ >= 0 && x_ < board.Length && y_ >=0 && y_ < board[0].Length && !visitedCells[x_][y_])
+            {
+                result |= Traverse(board, x_, y_, word, currentPosition, visitedCells);
+            }
+        }
+        
+        visitedCells[x][y] = false;
+
+        return result;
+    }
+}
---
+++ LeetCode/Topics/Graphs/Medium/WordSearch/Tests.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Topics.Graphs.Medium.WordSearch;
+
+public class Tests
+{
+    private readonly Solution _solution = new ();
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void WordSearchTest(char[][] board, string word, bool expectedExists)
+    {
+        
+        var actualExists = _solution.Exist(board, word);
+        
+        Assert.Equal(expectedExists, actualExists);
+    }
+    
+    public static IEnumerable<object[]> TestData => new List<object[]>()
+    {
+        new object[]
+        {
+            new [] { new[] {'A','B','C','E'}, ['S','F','C','S'], ['A','D','E','E'] },
+            "ABCB",
+            false
+        },
+        new object[]
+        {
+            new[]{new []{'A','B','C','E'}, ['S','F','C','S'], ['A','D','E','E']},
+            "ABCCED",
+            true
+        },
+        new object[]
+        {
+            new[]{new []{'A','B','C','E'}, ['S','F','C','S'], ['A','D','E','E']},
+            "SEE",
+            true
+        }
+    };
+}

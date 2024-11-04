ce67e3972656acca91184a5167032ec1ac39c02a
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:06:14 +0100

    adds string compression problem

---
+++ LeetCode/Topics/Strings/Medium/StringCompressionIII/Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Topics.Strings.Medium.StringCompressionIII;
+
+public class Tests
+{
+    public static IEnumerable<object[]> TestData => new List<object[]>
+    {
+        new object[]{"abc", "1a1b1c"}
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void Test(string input, string expected)
+    {
+        Solution solution = new Solution();
+        var actual = solution.CompressedString(input);
+        
+        Assert.Equal(expected, actual);
+    }
+}

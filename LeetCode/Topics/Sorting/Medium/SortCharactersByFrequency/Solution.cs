17a0fb2315c2f1debc097d07f68711d2f5a075ed
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:11:46 +0100

    Sort Characters By Frequency

---
+++ LeetCode/Topics/Sorting/Medium/SortCharactersByFrequency/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Topics.Sorting.Medium.SortCharactersByFrequency;
+
+/// <summary>
+/// 451. Sort Characters By Frequency
+/// </summary>
+/// <remarks>
+/// Time : O(n log(n) : Beats 61.9%
+/// Space : O(n) : Beats 74.41%
+/// </remarks>
+public class Solution 
+{
+    public string FrequencySort(string s)
+    {
+        var frequencies = new Dictionary<char, int>();
+
+        foreach(var ch in s)
+            if(!frequencies.TryAdd(ch, 1))
+                frequencies[ch]++;
+
+        return string
+            .Concat(frequencies
+            .OrderByDescending(x => x.Value)
+            .Select(x => new String(x.Key, x.Value)));
+    }
+}

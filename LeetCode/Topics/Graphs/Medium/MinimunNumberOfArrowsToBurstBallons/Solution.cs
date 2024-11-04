02923576275cc072097062a1a5da567d430934b0
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:07:08 +0100

    adds "Minimum number of arrows to burst balloons" problem

---
+++ LeetCode/Topics/Graphs/Medium/MinimunNumberOfArrowsToBurstBallons/Solution.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Topics.Graphs.Medium.MinimunNumberOfArrowsToBurstBallons;
+public class Solution {
+    public int FindMinArrowShots(int[][] points)
+    {
+        Array.Sort(points, (a, b) => a[0].CompareTo(b[0]));
+        
+        var lastPoint = points[0][1];
+        var arrows = 1;
+
+        foreach (var point in points)
+        {
+            if (point[0] > lastPoint)
+            {
+                arrows++;
+                lastPoint = point[1];
+            }
+            lastPoint = Math.Min(lastPoint, point[1]);
+        }
+        return arrows;
+    }
+}
---
+++ LeetCode/Topics/Graphs/Medium/MinimunNumberOfArrowsToBurstBallons/Tests.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Topics.Graphs.Medium.MinimunNumberOfArrowsToBurstBallons;
+
+public class Tests
+{
+    private readonly Solution _solution = new Solution();
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>()
+        {
+            new object[]{ new []{new[]{1,2}, [2,3], [3,4], [4,5]} , 2}
+        };
+    
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void DetectCycle(int[][] ballons, int numberOfArrows)
+    {
+        var actualNumOfArrows = _solution.FindMinArrowShots(ballons);
+        
+        Assert.Equal(numberOfArrows, actualNumOfArrows);
+    }
+}

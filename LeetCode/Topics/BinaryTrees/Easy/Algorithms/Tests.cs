8910d9e6d62744abdb18cebd2daff6d356079a46
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:14:55 +0100

    Tree traversal

---
+++ LeetCode/Topics/BinaryTrees/Easy/Algorithms/Tests.cs
@@ -0,0 +1,28 @@
+﻿using LeetCode.Topics.BinaryTrees.Easy.Algorithms.Traversals;
+
+namespace LeetCode.Topics.BinaryTrees.Easy.Algorithms;
+
+public class Tests
+{
+    [Fact]
+    public void TraversalTest()
+    {
+        var tree = new TreeNode(1);
+        tree.Left = new TreeNode(2);
+        tree.Right = new TreeNode(3);
+        tree.Left.Left = new TreeNode(4);
+        tree.Left.Right = new TreeNode(5);
+        tree.Right.Left = new TreeNode(6);
+        tree.Right.Right = new TreeNode(7);
+
+        var actualTraversal = new List<TreeNode>();
+        var expectedTraversal = new[] { 4, 5, 2, 6, 7, 3, 1 };
+        
+        BinaryTreePostOrder.Traverse(tree, actualTraversal);
+        
+        
+        Assert.True(actualTraversal.Select(x => x.Val).SequenceEqual(expectedTraversal));
+        
+        // Console.WriteLine(string.Join(" -    > ",actualTraversal.Select(x => x.Val)));
+    }
+}
---
+++ LeetCode/Topics/BinaryTrees/Easy/Algorithms/Traversals/BinaryTreePostOrder.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Topics.BinaryTrees.Easy.Algorithms.Traversals;
+
+public static class BinaryTreePostOrder
+{
+    public static void Traverse(TreeNode root, List<TreeNode> traversal)
+    {
+        if (root.Left != null)
+        {
+            Traverse(root.Left, traversal);
+        }
+
+        if (root.Right != null)
+        {
+            Traverse(root.Right, traversal);
+        }
+
+        if (traversal != null)
+        {
+            traversal.Add(root);
+        }
+        else
+        {
+            Console.Write(root.Val + " ");
+        }
+    }
+}

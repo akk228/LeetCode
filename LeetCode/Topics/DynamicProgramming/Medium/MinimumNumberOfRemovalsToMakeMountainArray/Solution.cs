9765800d5a974baf49e7c5335e06dab17dfcda53
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:10:00 +0100

    Minimum number of removals to Make Mountain array

---
+++ LeetCode/Topics/DynamicProgramming/Medium/MinimumNumberOfRemovalsToMakeMountainArray/Solution.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.MinimumNumberOfRemovalsToMakeMountainArray;
+
+public class Solution {
+    private class Node(int value, int increasingSequence = 0, int decreasingSequence = 0)
+    {
+        public int Value { get; } = value;
+        public int IncreasingSequence { get; set; } = increasingSequence;
+        public int DecreasingSequence { get; set; } = decreasingSequence;
+        public bool IsMountain => IncreasingSequence > 0 && DecreasingSequence > 0;
+        public int MountainLength => IsMountain ? IncreasingSequence + DecreasingSequence + 1 : 0;
+    }
+    
+    public int MinimumMountainRemovals(int[] nums)
+    {
+        var topHills = new Node[nums.Length];
+        
+        topHills[0] = new (nums[0]);
+        topHills[^1] = new (nums[^1]);
+
+        for (var i = 1; i < nums.Length - 1; i++)
+        {
+            var maxIncreasingSequence = 0;
+            var steepExists = false;
+            for (int j = 0; j < i; j++)
+            {
+                if (topHills[j].Value >= nums[i]) continue;
+                steepExists = true;
+                    
+                if(topHills[j].IncreasingSequence > maxIncreasingSequence)
+                    maxIncreasingSequence = topHills[j].IncreasingSequence;
+            }
+            
+            topHills[i] = new (nums[i], !steepExists ? 0 : maxIncreasingSequence + 1);
+        }
+
+        for (var i = nums.Length - 2; i > 0; i--)
+        {
+            var maxDecreasingSequence = 0;
+            var steepExists = false;
+            
+            for (var j = nums.Length - 1; j > i; j--)
+            {
+                if (topHills[j].Value >= nums[i]) continue;
+                steepExists = true;
+                if ( topHills[j].DecreasingSequence > maxDecreasingSequence)
+                    maxDecreasingSequence = topHills[j].DecreasingSequence;
+            }
+            
+            topHills[i].DecreasingSequence = !steepExists ? 0 : maxDecreasingSequence + 1;
+        }
+        
+        var minDeletions = nums.Length;
+
+        foreach (var t in topHills)
+        {
+            var deletions = topHills.Length - t.MountainLength;
+            if(deletions < minDeletions && t.IsMountain) minDeletions = deletions;
+        }
+        
+        return minDeletions;
+    }
+}
---
+++ LeetCode/Topics/DynamicProgramming/Medium/MinimumNumberOfRemovalsToMakeMountainArray/Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.MinimumNumberOfRemovalsToMakeMountainArray;
+
+public class MinimumNumberOfRemovalsToMakeMountainArrayTests
+{
+    private readonly Solution _solution = new();
+    public static IEnumerable<object[]> TestData() => new List<object[]>(new []
+    {
+        new object[]{ new []{2,1,1,5,6,2,3,1}, 3},
+        new object[]{ new []{1,3,1}, 0},
+        new object[]{ new []{100,92,89,77,74,66,64,66,64}, 6}
+    });
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void FoundCorrectNumberOfRemovals(int[] nums, int expected)
+    {
+        var minNumberOfRemovalsToMakeMountainArray = _solution.MinimumMountainRemovals(nums);
+        Assert.Equal(expected, minNumberOfRemovalsToMakeMountainArray);
+    }
+}

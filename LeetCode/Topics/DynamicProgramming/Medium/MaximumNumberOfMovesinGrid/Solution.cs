b7c80ab0b3a6ced63422e0b8769e3c446bfbf439
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:09:28 +0100

    "Maximum number of Moves in the grid"

---
+++ LeetCode/Topics/DynamicProgramming/Medium/MaximumNumberOfMovesinGrid/Solution.cs
@@ -0,0 +1,71 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.MaximumNumberOfMovesinGrid;
+
+
+public class Solution
+{
+    private readonly int[][] _directions = {
+        [1, 1],
+        [-1, 1],
+        [0, 1]
+    };
+    
+    public int MaxMoves(int[][] grid) {
+        var maxPath = 0;
+        var visited = new bool[grid.Length][];
+        
+        for (var row = 0; row < grid.Length; row++)
+        {
+            visited[row] = new bool[grid[0].Length];
+        }
+        
+        for(var i = 0; i < grid.Length; i++){
+            var path = FindMaxPath(grid, i, 0, 0, visited);
+            
+            if (path == grid[0].Length - 1)
+            {
+                return grid[0].Length - 1;
+            }
+
+            if (path > maxPath)
+            {
+                maxPath = path;
+            }
+        }
+
+        return maxPath;
+    }
+
+    private int FindMaxPath(int[][] grid, int x0, int y0, int currentPath, bool[][] visited)
+    {
+        var maxPath = currentPath;
+        
+        foreach(var direction in _directions){
+            var x = x0 + direction[0];
+            var y = y0 + direction[1];
+
+            if (x < 0 ||
+                x >= grid.Length ||
+                y >= grid[0].Length ||
+                grid[x][y] <= grid[x0][y0] ||
+                visited[x][y])
+            {
+                continue;
+            }
+            
+            visited[x][y] = true;
+            
+            var result = FindMaxPath(grid, x, y, currentPath + 1, visited);
+            
+            if (result == grid[0].Length - 1)
+            {
+                return grid[0].Length - 1;
+            }
+
+            if (result > maxPath)
+            {
+                maxPath = result;
+            }
+        }
+        return maxPath;
+    }
+}
---
+++ LeetCode/Topics/DynamicProgramming/Medium/MaximumNumberOfMovesinGrid/Tests.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.MaximumNumberOfMovesinGrid;
+
+public class Tests
+{
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]{ new int[][]{[2,4,3,5],[5,4,9,3],[3,4,2,11],[10,9,13,15]}, 3},
+        };
+    private readonly Solution solution = new Solution();
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void FindLongestSquareStreakInAnArray(int[][] grid, int expectedMaxPath)
+    {
+        var result = solution.MaxMoves(grid);
+        
+        Assert.Equal(expectedMaxPath, result);
+    }
+}

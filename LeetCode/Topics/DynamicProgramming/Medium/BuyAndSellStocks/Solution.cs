d4ed5d64e739f0dcb751ffa78d1cd0476604dd38
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:07:35 +0100

    adds buy and sell stocks problem

---
+++ LeetCode/Topics/DynamicProgramming/Medium/BuyAndSellStocks/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.BuyAndSellStocks;
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        var maxProfit = 0;
+        
+        for (var i = 0; i < prices.Length; i++)
+        {
+            var currentProfit = GetProfit(i, i, prices);
+            maxProfit = Math.Max(maxProfit, currentProfit);
+        }
+
+        return maxProfit;
+    }
+
+    private int GetProfit(int dayBought, int currentDay, int[] prices)
+    {
+        if (dayBought == prices.Length - 1) return 0;
+        if (currentDay == prices.Length - 1) return prices[currentDay] - prices[dayBought];
+        
+        var profitIfSoldToday = 
+            prices[currentDay] - prices[dayBought] +
+            GetProfit(currentDay + 1, currentDay + 1, prices);
+        
+        var profitIfSoldLater = 0;
+        
+        for (var i = currentDay + 1; i < prices.Length; i++)
+        {
+            profitIfSoldLater = Math.Max(
+                GetProfit(dayBought, i, prices),
+                profitIfSoldLater
+                );
+        }
+        
+        
+        return Math.Max(profitIfSoldToday, profitIfSoldLater);
+    }
+}
---
+++ LeetCode/Topics/DynamicProgramming/Medium/BuyAndSellStocks/Tests.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.BuyAndSellStocks;
+
+public class Tests
+{
+    private readonly Solution _solution = new();
+
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void TestCase(int[] prices, int expectedProfit)
+    {
+        var result = _solution.MaxProfit(prices);
+        
+        Assert.Equal(expectedProfit, result);
+    }
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]{ new []{1}, 0},
+            new object[]{ new []{1,2}, 1},
+            new object[]{ new []{3,1}, 0},
+            new object[]{ new []{1,2,3}, 2},
+            new object[]{ new []{1,5,10,1}, 9},
+            new object[]{ new []{1,2,3,4,5}, 4},
+            new object[]{ new []{7,1,5,3,6,4}, 7},
+        };
+}

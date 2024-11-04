1c9cf78e06c2656978027f0f726405c88cee0df8
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:08:06 +0100

    adds "Longest square strike" problem

---
+++ LeetCode/Topics/DynamicProgramming/Medium/LongestSquareStreakInAnArray/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.LongestSquareStreakInAnArray;
+
+public class Solution {
+    public int LongestSquareStreak(int[] nums)
+    {
+        
+        Array.Sort(nums);
+        var squareStrikes = new Dictionary<int, int>();
+
+        foreach (var num in nums)
+        {
+            var square = num * num;
+
+            if (squareStrikes.ContainsKey(num))
+            {
+                squareStrikes.TryAdd(square, squareStrikes[num] + 1);
+            }
+            else
+            {
+                squareStrikes.TryAdd(square, 1);
+            }
+        }
+
+        var max = squareStrikes.Values.Max();
+        
+        return max > 1 ? max : -1;
+    }
+}
---
+++ LeetCode/Topics/DynamicProgramming/Medium/LongestSquareStreakInAnArray/Tests.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Topics.DynamicProgramming.Medium.LongestSquareStreakInAnArray;
+
+public class Tests
+{
+    public static IEnumerable<object[]> Data =>
+        new List<object[]>
+        {
+            new object[]{ new []{4, 3, 6, 16, 8, 2} , 3},
+            new object[]{ new []{2,3,5,6,7}, -1},
+            new object[]{ new []{1}, -1},
+            new object[]{ new []{1,2,2,4}, 2},
+        };
+    private readonly Solution solution = new Solution();
+    [Theory]
+    [MemberData(nameof(Data))]
+    public void FindLongestSquareStreakInAnArray(int[] nums, int squareStreak)
+    {
+        var result = solution.LongestSquareStreak(nums);
+        
+        Assert.Equal(squareStreak, result);
+    }
+}

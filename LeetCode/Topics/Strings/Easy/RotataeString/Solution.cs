b14ac4d1c410aada1ea8a6597e4778aa55056d84
Andrei Kovtun <andreykpq@gmail.com>
2024-11-04 13:11:16 +0100

    Rotate String

---
+++ LeetCode/Topics/Strings/Easy/RotataeString/Solution.cs
@@ -0,0 +1,50 @@
+﻿namespace LeetCode.Topics.Strings.Easy.RotataeString;
+
+/// <summary>
+/// 796. Rotate String
+/// </summary>
+public interface ISolution
+{
+    bool RotateString(string s, string goal);
+}
+
+/// <remarks>
+/// Time : O(s.Length^2)
+/// Space : O(1)
+/// </remarks>
+public class SquareSolution : ISolution 
+{
+    public bool RotateString(string s, string goal) {
+        if(s.Length != goal.Length) return false;
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (s[i] != goal[0]) continue;
+            
+            int matchingLength = 1, position = 1;
+
+            while (s[(i + position) % s.Length] == goal[position] && matchingLength < s.Length)
+            {
+                matchingLength++;
+                position = (position + 1) % goal.Length;
+            }
+
+            if (matchingLength == s.Length) return true;
+            if (matchingLength > (s.Length + 1) / 2) return false;
+        }
+
+        return false;
+    }
+}
+
+/// <remarks>
+/// Time : O(s.Length)
+/// Space : O(s.Length)
+/// </remarks>
+public class LinearSolution : ISolution
+{
+    public bool RotateString(string s, string goal) {
+        if(s.Length != goal.Length) return false;
+        return (s + s).Contains(goal);
+    }
+}

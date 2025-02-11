d0e0890ed6e4fad20a79a09f75ccacd9c783e015
Andrei Kovtun <andreykpq@gmail.com>
2025-02-11 19:37:02 +0100

    Adds KMP algorithm

---
+++ LeetCode/Algos/StringMatching/LongestPrefixSuffix/KMP.cs
@@ -0,0 +1,40 @@
+namespace LeetCode.Algos.StringMatching.LongestPrefixSuffix;
+
+public class KMP
+{
+    // Your implementation will go here
+    public static int[] LPS(in string pattern)
+    {
+        var lps = new int[pattern.Length];
+        lps[0] = 0;
+        var len = 0;
+        var i = 1;
+
+        while (i < pattern.Length)
+        {
+            if (pattern[i] == pattern[len])
+            {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else
+            {
+                if (len != 0)
+                {
+                    len = lps[len - 1];
+                }
+                else
+                {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
+}
+
+
+// b a a b a a b a a b a a
+// 
---
+++ LeetCode/Algos/StringMatching/LongestPrefixSuffix/KMPTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LeetCode.Algos.StringMatching.LongestPrefixSuffix
+{
+    public class KMPTests
+    {
+        public static IEnumerable<object[]> TestData()
+        {
+            yield return new object[] { "AAACAAAA", new int[] { 0, 1, 2, 0, 1, 2, 3, 3 } };
+            yield return new object[] { "ABCDE", new int[] { 0, 0, 0, 0, 0 } };
+            yield return new object[] { "AABAACAABAA", new int[] { 0, 1, 0, 1, 2, 0, 1, 2, 3, 4, 5 } };
+            yield return new object[] { "AAABAAA", new int[] { 0, 1, 2, 0, 1, 2, 3 } };
+            yield return new object[] { "ABABAC", new int[] { 0, 0, 1, 2, 3, 0 } };
+            yield return new object[] { "baabaabaabaa", new int[] { 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 } };
+        }
+
+        [Theory]
+        [MemberData(nameof(TestData))]
+        public void LPS_Tests(string pattern, int[] expected)
+        {
+            int[] result = KMP.LPS(pattern);
+            Assert.Equal(expected, result);
+        }
+    }
+}

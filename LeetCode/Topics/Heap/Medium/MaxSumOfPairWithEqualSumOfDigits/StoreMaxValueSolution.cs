d7b20d20d19627fb98907b15775fe453f949943e
Andrei Kovtun <andreykpq@gmail.com>
2025-02-12 14:01:28 +0100

    updates namespaces to be file scoped

--- LeetCode/Topics/Heap/Medium/MaxSumOfPairWithEqualSumOfDigits/StoreMaxValueSolution.cs
+++ LeetCode/Topics/Heap/Medium/MaxSumOfPairWithEqualSumOfDigits/StoreMaxValueSolution.cs
@@ -1,42 +1,38 @@
-using System.Collections.Generic;
-using System.Linq;
+namespace Studying.LeetCode.Topics.Heap.Medium.MaxSumOfPairWithEqualSumOfDigits;
 
-namespace Studying.LeetCode.Topics.Heap.Medium.MaxSumOfPairWithEqualSumOfDigits
+public class StoreMaxValueSolution : ISolution
 {
-    public class StoreMaxValueSolution : ISolution
+    public int MaximumSum(int[] nums)
     {
-        public int MaximumSum(int[] nums)
+        var digitSumDict = new int[82];
+
+        var maxSum = -1;
+
+        foreach (var num in nums)
         {
-            var digitSumDict = new int[82];
+            var digitSum = GetDigitSum(num);
 
-            var maxSum = -1;
-
-            foreach (var num in nums)
+            if (digitSumDict[digitSum] == 0)
             {
-                var digitSum = GetDigitSum(num);
-
-                if (digitSumDict[digitSum] == 0)
-                {
-                    digitSumDict[digitSum] = num;
-                    continue;
-                }
-
-                maxSum = Math.Max(num + digitSumDict[digitSum], maxSum);
-                digitSumDict[digitSum] = Math.Max(num, digitSumDict[digitSum]);
+                digitSumDict[digitSum] = num;
+                continue;
             }
 
-            return maxSum;
+            maxSum = Math.Max(num + digitSumDict[digitSum], maxSum);
+            digitSumDict[digitSum] = Math.Max(num, digitSumDict[digitSum]);
         }
 
-        private int GetDigitSum(int num)
+        return maxSum;
+    }
+
+    private int GetDigitSum(int num)
+    {
+        int sum = 0;
+        while (num > 0)
         {
-            int sum = 0;
-            while (num > 0)
-            {
-                sum += num % 10;
-                num /= 10;
-            }
-            return sum;
+            sum += num % 10;
+            num /= 10;
         }
+        return sum;
     }
 }
--- LeetCode/Topics/Heap/Medium/MaxSumOfPairWithEqualSumOfDigits/Tests.cs
+++ LeetCode/Topics/Heap/Medium/MaxSumOfPairWithEqualSumOfDigits/Tests.cs
@@ -1,33 +1,32 @@
 using System.Diagnostics.CodeAnalysis;
 
-namespace Studying.LeetCode.Topics.Heap.Medium.MaxSumOfPairWithEqualSumOfDigits
-{
-    [ExcludeFromCodeCoverage]
-    public class Tests
-    {
-        public static IEnumerable<object[]> TestData =>
-            new List<object[]>
-            {
-                new object[] { new int[] { 51, 71, 17, 42 }, 93 },
-                new object[] { new int[] { 42, 33, 60 }, 102 },
-                new object[] { new int[] { 51, 32, 43 }, -1 }
-            };
+namespace Studying.LeetCode.Topics.Heap.Medium.MaxSumOfPairWithEqualSumOfDigits;
 
-        private readonly IEnumerable<ISolution> _solutions = new List<ISolution>
+[ExcludeFromCodeCoverage]
+public class Tests
+{
+    public static IEnumerable<object[]> TestData =>
+        new List<object[]>
         {
-            new PriorityQueueSolution(),
-            new StoreMaxValueSolution()
+            new object[] { new int[] { 51, 71, 17, 42 }, 93 },
+            new object[] { new int[] { 42, 33, 60 }, 102 },
+            new object[] { new int[] { 51, 32, 43 }, -1 }
         };
 
-        [Theory]
-        [MemberData(nameof(TestData))]
-        public void TestMaximumSum(int[] nums, int expected)
+    private readonly IEnumerable<ISolution> _solutions = new List<ISolution>
+    {
+        new PriorityQueueSolution(),
+        new StoreMaxValueSolution()
+    };
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void TestMaximumSum(int[] nums, int expected)
+    {
+        foreach (var solution in _solutions)
         {
-            foreach (var solution in _solutions)
-            {
-                int result = solution.MaximumSum(nums);
-                Assert.Equal(expected, result);
-            }
+            int result = solution.MaximumSum(nums);
+            Assert.Equal(expected, result);
         }
     }
 }
